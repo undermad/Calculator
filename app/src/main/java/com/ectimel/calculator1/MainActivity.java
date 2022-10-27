31046823f79c5c8fa2a4f857ec64a97e0afacefd
Ectimel <dtworek94@gmail.com>
2022-10-27 12:08:59 +0100

    git is working, test complete

--- app/src/main/java/com/ectimel/calculator1/MainActivity.java
+++ app/src/main/java/com/ectimel/calculator1/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
